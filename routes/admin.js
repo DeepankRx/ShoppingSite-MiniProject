c3439184b3a74c8494048d766cf6fbbfc1785361
DeepankRx <deepankpushpad10@gmail.com>
2022-04-24 00:30:15 +0530

    following MVC creating controller

--- routes/admin.js
+++ routes/admin.js
@@ -2,23 +2,15 @@ const path = require("path");
 
 const express = require("express");
 
-const rootDir = require("../util/path");
+const productsController = require('../controllers/products');
+const { builtinModules } = require("module");
 
 const router = express.Router();
 
-const products = [];
 
-router.get("/add-product", (req, res, next) => {
-  res.render("add-product", {
-    pageTitle: "Add Product",
-    path: "/admin/add-product",
-  });
-});
+router.get("/add-product", productsController.getAddProduct);
 
-router.post("/add-product", (req, res, next) => {
-  products.push({ title: req.body.title });
-  res.redirect("/");
-});
+router.post("/add-product", productsController.postAddProduct);
 
-exports.routes = router;
-exports.products = products;
+
+module.exports = router;
--- routes/shop.js
+++ routes/shop.js
@@ -2,14 +2,10 @@ const path = require("path");
 
 const express = require("express");
 
-const rootDir = require("../util/path");
 const adminData = require("./admin");
-
+const productsController = require('../controllers/products')
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-  const products = adminData.products;
-  res.render("shop", { prods: products, pageTitle: "Shop", path: "/" });
-});
+router.get("/",productsController.getProducts);
 
 module.exports = router;
