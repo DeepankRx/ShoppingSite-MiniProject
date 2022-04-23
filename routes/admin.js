2bf4da6ffcef41218e61f2ce2ea0f4766af701dc
DeepankRx <deepankpushpad10@gmail.com>
2022-04-23 14:34:40 +0530

    serving html pages

--- routes/admin.js
+++ routes/admin.js
@@ -1,17 +1,15 @@
 const express = require("express");
 const router = express.Router();
+const path = require("path");
 
-
-router.post("/product", (req, res, next) => {
+router.post("/add-product", (req, res, next) => {
   console.log(req.body);
   res.redirect("/");
 });
 
 
 router.get("/add-product", (req, res, next) => {
-  res.status(200).send(
-    "<form action='/admin/product' method='POST'><input type='text' name='title' placeholder='Enter the product'></input><button type='submit'>Submit    </button></form>"
-  );
+  res.status(200).sendFile(path.join(__dirname, "../", "views", "add-product.html"));
 });
 
 module.exports = router;
--- routes/shop.js
+++ routes/shop.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
-
+const path = require("path");
 router.get("/", (req, res, next) => {
-    res.status(200).send("<h1>Hello From Express</h1>");
-  });
+    res.status(200).sendFile(path.join(__dirname,'../','views','shop.html'))
+});
 
 module.exports = router;
