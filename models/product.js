bb520171096973875fbe8629993f1f1ed804dc7d
DeepankRx <deepankpushpad10@gmail.com>
2022-04-24 01:08:31 +0530

    enhancing the code

--- models/product.js
+++ models/product.js
@@ -21,21 +21,21 @@ module.exports = class Product {
   }
 
   save() {
-    const p = path.join(
-      path.dirname(process.mainModule.filename),
-      "data",
-      "products.json"
-    );
-    fs.readFile(p, (err, fileContent) => {
-      let products = [];
-      if (!err) {
-        products = JSON.parse(fileContent);
-      }
-      products.push(this);
-      fs.writeFile(p, JSON.stringify(products), (err) => {
-        console.log(err);
-      });
-    });
+   getProductsFromFile(products => {
+       products.push(this);
+         fs.writeFile(
+              path.join(
+                path.dirname(process.mainModule.filename),
+                "data",
+                "products.json"
+                ),
+                JSON.stringify(products),
+                err => {
+                    console.log(err);
+                    }
+                );
+            });
+            
   }
 
   static fetchAll(cb) {
