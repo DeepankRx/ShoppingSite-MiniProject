7de995b972737892824bc66f82f83b1ae61519ca
DeepankRx <deepankpushpad10@gmail.com>
2022-04-23 13:28:21 +0530

    filtering routes

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 const http = require("http");
 
 const express = require("express");
-
+const bodyParser = require("body-parser");
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.post('/product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/');
+})
 
-
-app.use("/add-product", (req, res, next) => {
-  console.log("In the another middleware");
+app.use("/add-product", (req, res, next) => {   
   res.send(
     "<form action='/product' method='POST'><input type='text' name='title' placeholder='Enter the product'></input><button type='submit'>Submit    </button></form>"
   );
 });
 app.use("/", (req, res, next) => {
-  console.log("In the middleware");
   res.send("<h1>Hello From Express</h1>");
 
 });
