fb2e1f8b0220b55f8b732f31020593b92700952d
DeepankRx <deepankpushpad10@gmail.com>
2022-04-24 12:05:24 +0530

    add to cart details fethced

--- routes/shop.js
+++ routes/shop.js
@@ -12,6 +12,8 @@ router.get('/products', shopController.getProducts);
 
 router.get('/products/:productId',shopController.getProduct);
 
+router.post('/cart',shopController.postCart);
+
 router.get('/cart', shopController.getCart);
 
 router.get('/orders', shopController.getOrders);
