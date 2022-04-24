def01e26c09bff7e1a0c85c2d6be627d3a10e5bb
DeepankRx <deepankpushpad10@gmail.com>
2022-04-24 12:26:40 +0530

    add to cart functionality added

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,5 @@
 const Product = require('../models/product');
-
+const Cart = require('../models/cart');
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
     res.render('shop/product-list', {
@@ -41,6 +41,9 @@ exports.getCart = (req, res, next) => {
 
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
+  Product.findById(prodId,(product) => {
+      Cart.addProduct(prodId,product.price);
+  })
   console.log(prodId);
   res.redirect('/cart')
 }
