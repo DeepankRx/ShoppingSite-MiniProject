eea0dab7010ff1c91ed2393449a21b5954d2a7b8
DeepankRx <deepankpushpad10@gmail.com>
2022-04-24 11:52:55 +0530

    added detail of product

--- controllers/shop.js
+++ controllers/shop.js
@@ -14,8 +14,12 @@ exports.getProducts = (req, res, next) => {
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
   Product.findById(prodId,product =>{
-    console.log(product);
-  })
+    res.render('shop/product-detail', {
+      product: product,
+      pageTitle : product.title,
+      path:'/products'
+    })
+  })  
 }
 
 exports.getIndex = (req, res, next) => {
