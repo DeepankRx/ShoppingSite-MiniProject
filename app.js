33b8bd15690d7dfc73ab6e56b218f3cc25fabdb3
DeepankRx <deepankpushpad10@gmail.com>
2022-05-03 17:03:33 +0530

    creating a user

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const errorController = require("./controllers/error");
-
+const User = require("./models/user");
 const app = express();
 
 app.set("view engine", "ejs");
@@ -16,6 +16,15 @@ const shopRoutes = require("./routes/shop");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use((req, res, next) => {
+  User.findById("627112e0d4bd9e666d0c68f2")
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
+});
+
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
@@ -23,8 +32,20 @@ app.use(errorController.get404);
 mongoose
   .connect("mongodb://localhost:27017/ecommerce")
   .then((result) => {
-    app.listen(3000,()=>{
-        console.log("Server started at 3000");
+    User.findOne().then((user) => {
+      if (!user) {
+        const user = new User({
+          name: "Deepank",
+          email: " deepankrx@gmail.com",
+          cart: {
+            items: [],
+          },
+        });
+        user.save();
+      }
+    });
+    app.listen(3000, () => {
+      console.log("Server started at 3000");
     });
   })
   .catch((err) => console.log(err));
---
+++ models/user.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    cart: {
+        items: [
+            {
+                productId: {
+                    type: Schema.Types.ObjectId,
+                    ref: "Product",
+                    required: true
+                },
+                quantity: {
+                    type: Number,
+                    required: true
+                }
+            }
+        ]
+    }
+});
+
+module.exports = mongoose.model("User", userSchema);
