4428fde5cb7a528d8ec71f0b698b1d2cb1bba285
DeepankRx <deepankpushpad10@gmail.com>
2022-05-03 16:48:49 +0530

    edit product

--- controllers/admin.js
+++ controllers/admin.js
@@ -35,7 +35,7 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   const prodId = req.params.productId;
-  Product.findById(prodId, product => {
+  Product.findById(prodId).then( product => {
     if (!product) {
       return res.redirect('/');
     }
@@ -45,6 +45,9 @@ exports.getEditProduct = (req, res, next) => {
       editing: editMode,
       product: product
     });
+  })
+  .catch(err => {
+    console.log(err);
   });
 };
 
@@ -54,24 +57,31 @@ exports.postEditProduct = (req, res, next) => {
   const updatedPrice = req.body.price;
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
-  const updatedProduct = new Product(
-    prodId,
-    updatedTitle,
-    updatedImageUrl,
-    updatedDesc,
-    updatedPrice
-  );
-  updatedProduct.save();
-  res.redirect('/admin/products');
+  Product.findById(prodId).then( product => {
+    product.title = updatedTitle;
+    product.price = updatedPrice;
+    product.imageUrl = updatedImageUrl;
+    product.description = updatedDesc;
+    return product.save();
+  })
+  .then(() => {
+    console.log('Product Updated');
+    res.redirect('/admin/products');
+  })
+  .catch(err => {
+    console.log(err);
+  });
 };
-
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
+  Product.find().then(products => {
     res.render('admin/products', {
       prods: products,
       pageTitle: 'Admin Products',
       path: '/admin/products'
     });
+  })
+  .catch(err => {
+    console.log(err);
   });
 };
 
