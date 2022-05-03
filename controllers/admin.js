a8f8ca22daaefd3a18fe6b6f85962de48281eb70
DeepankRx <deepankpushpad10@gmail.com>
2022-05-03 16:50:32 +0530

    delete product

--- controllers/admin.js
+++ controllers/admin.js
@@ -87,6 +87,12 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.deleteById(prodId);
-  res.redirect('/admin/products');
+  Product.findByIdAndRemove(prodId)
+  .then(() => {
+    console.log('Product Deleted');
+    res.redirect('/admin/products');
+  })
+  .catch(err => {
+    console.log(err);
+  }); 
 };
