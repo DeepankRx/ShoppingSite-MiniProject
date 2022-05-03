97c5d7ee4656dc6fe07940e5f2864fa3b764c240
DeepankRx <deepankpushpad10@gmail.com>
2022-05-03 16:42:27 +0530

    fetching products from mongoose

--- controllers/shop.js
+++ controllers/shop.js
@@ -2,13 +2,16 @@ const Product = require('../models/product');
 const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
+  Product.find().then(products => {
     res.render('shop/product-list', {
       prods: products,
       pageTitle: 'All Products',
       path: '/products'
     });
-  });
+  })
+  .catch(err => {
+    console.log(err);
+  }); 
 };
 
 exports.getProduct = (req, res, next) => {
@@ -23,12 +26,15 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll(products => {
+  Product.find().then(products => {
     res.render('shop/index', {
       prods: products,
       pageTitle: 'Shop',
       path: '/'
     });
+  })
+  .catch(err => {
+    console.log(err);
   });
 };
 
