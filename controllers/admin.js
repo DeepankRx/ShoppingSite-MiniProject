0f930f4dff412eac1c38389912195e59896ba43b
DeepankRx <deepankpushpad10@gmail.com>
2022-05-03 16:38:21 +0530

    adding product in mongoose

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,9 +13,20 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(null, title, imageUrl, description, price);
-  product.save();
-  res.redirect('/');
+  const product = new Product({
+    title: title,
+    price: price,
+    imageUrl: imageUrl,
+    description: description
+  });
+  product.save()
+  .then(() => {
+    console.log('Product Created');
+    res.redirect('/');
+  })
+  .catch(err => {
+    console.log(err);
+  })
 };
 
 exports.getEditProduct = (req, res, next) => {
