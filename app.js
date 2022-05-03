2162d3da1b10bb1bab30cb9d633436fccb732d9d
DeepankRx <deepankpushpad10@gmail.com>
2022-05-03 16:29:27 +0530

    switching to mongoose

--- app.js
+++ app.js
@@ -1,24 +1,30 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
-const bodyParser = require('body-parser');
-
-const errorController = require('./controllers/error');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const errorController = require("./controllers/error");
 
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+app.set("view engine", "ejs");
+app.set("views", "views");
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/admin', adminRoutes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
-
-app.listen(3000);
+mongoose
+  .connect("mongodb://localhost:27017/ecommerce")
+  .then((result) => {
+    app.listen(3000,()=>{
+        console.log("Server started at 3000");
+    });
+  })
+  .catch((err) => console.log(err));
