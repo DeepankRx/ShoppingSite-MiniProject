fd4e6cef80e6b81f4d7f32c448d642730cbb9be0
DeepankRx <deepankpushpad10@gmail.com>
2022-05-03 16:43:52 +0530

    fetching single product from mongoose

--- controllers/shop.js
+++ controllers/shop.js
@@ -16,12 +16,15 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  Product.findById(prodId, product => {
+  Product.findById(prodId).then( product => {
     res.render('shop/product-detail', {
       product: product,
       pageTitle: product.title,
       path: '/products'
     });
+  })
+  .catch(err => {
+    console.log(err);
   });
 };
 
