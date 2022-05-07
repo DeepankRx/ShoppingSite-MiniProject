748f90c37f6bf6bea535dd3b72415e19f6a3b99a
DeepankRx <deepankpushpad10@gmail.com>
2022-05-07 21:24:53 +0530

    using mongodb session storage to store sessions

--- app.js
+++ app.js
@@ -6,8 +6,13 @@ const mongoose = require("mongoose");
 const session = require("express-session");
 const errorController = require("./controllers/error");
 const User = require("./models/user");
-
+const MongoDbStore = require("connect-mongodb-session")(session);
 const app = express();
+const MONGODB_URI = "mongodb://localhost:27017/ecommerce";
+const store = new MongoDbStore({
+  uri: MONGODB_URI,
+  collection: "sessions",
+});
 
 app.set("view engine", "ejs");
 app.set("views", "views");
@@ -18,17 +23,20 @@ const authRoutes = require("./routes/auth");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
-app.use(session({
-  secret: "my secret",
-  resave: false,
-  saveUninitialized: false,
-  }));
+app.use(
+  session({
+    secret: "my secret",
+    resave: false,
+    saveUninitialized: false,
+    store: store,
+  })
+);
 app.use((req, res, next) => {
   User.findById("5bab316ce0a7c75f783cb8a8")
     .then((user) => {
       req.user = user;
       next();
-    })  
+    })
     .catch((err) => console.log(err));
 });
 
