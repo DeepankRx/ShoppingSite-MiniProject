882e3e4b9bb46af38213e566219e364f3e739fb1
DeepankRx <deepankpushpad10@gmail.com>
2022-05-07 21:06:15 +0530

    login cookie

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,12 +1,21 @@
 exports.getLogin = (req, res, next) => {
+    
+    let isLoggedIn = req.get('Cookie').split('=')[1];
+
+
+    if (isLoggedIn === 'true') {
+        isLoggedIn = true;
+    } else {
+        isLoggedIn = false;
+    }
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
-    isAuthenticated: req.isLoggedIn
+    isAuthenticated: isLoggedIn
   });
 };
 
 exports.postLogin = (req, res, next) => {
-    req.isLoggedIn = true;
+    res.setHeader('Set-Cookie', 'loggedIn=true');
     res.redirect('/');
   };
