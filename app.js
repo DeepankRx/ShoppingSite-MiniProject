75c221c0dd55a1d8ffe759da7ef06011d9081a64
DeepankRx <deepankpushpad10@gmail.com>
2022-05-07 21:33:53 +0530

    Login / Logout functionality

--- app.js
+++ app.js
@@ -1,75 +1,80 @@
-const path = require("path");
+const path = require('path');
 
-const express = require("express");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
-const session = require("express-session");
-const errorController = require("./controllers/error");
-const User = require("./models/user");
-const MongoDbStore = require("connect-mongodb-session")(session);
-const app = express();
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
+
+const errorController = require('./controllers/error');
+const User = require('./models/user');
+
 const MONGODB_URI = "mongodb://localhost:27017/ecommerce";
-const store = new MongoDbStore({
+
+const app = express();
+const store = new MongoDBStore({
   uri: MONGODB_URI,
-  collection: "sessions",
+  collection: 'sessions'
 });
 
-app.set("view engine", "ejs");
-app.set("views", "views");
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-const adminRoutes = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
-const authRoutes = require("./routes/auth");
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(
   session({
-    secret: "my secret",
+    secret: 'my secret',
     resave: false,
     saveUninitialized: false,
-    store: store,
+    store: store
   })
 );
+
 app.use((req, res, next) => {
-  User.findById("5bab316ce0a7c75f783cb8a8")
-    .then((user) => {
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
+    .then(user => {
       req.user = user;
       next();
     })
-    .catch((err) => console.log(err));
+    .catch(err => console.log(err));
 });
 
-app.use("/admin", adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
 
 app.use(errorController.get404);
 
 mongoose
-  .connect("mongodb://localhost:27017/ecommerce", {
+  .connect(MONGODB_URI,{
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then((result) => {
-    User.findOne().then((user) => {
+  .then(result => {
+    User.findOne().then(user => {
       if (!user) {
         const user = new User({
-          name: "Max",
-          email: "max@test.com",
+          name: 'Max',
+          email: 'max@test.com',
           cart: {
-            items: [],
-          },
+            items: []
+          }
         });
         user.save();
       }
     });
-    app
-      .listen(3000, () => {
-        console.log("Server started on port 3000");
-      })
-      .catch((err) => console.log(err));
+    app.listen(3000,()=>{
+      console.log('App Is Running On 3000')
+    });
   })
-  .catch((err) => {
+  .catch(err => {
     console.log(err);
   });
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,21 +1,29 @@
+const User = require('../models/user');
+
 exports.getLogin = (req, res, next) => {
-    
-    let isLoggedIn = req.get('Cookie').split('=')[1];
-
-
-    if (isLoggedIn === 'true') {
-        isLoggedIn = true;
-    } else {
-        isLoggedIn = false;
-    }
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
-    isAuthenticated: isLoggedIn
+    isAuthenticated: false
   });
 };
 
 exports.postLogin = (req, res, next) => {
-    req.session.isLoggedIn = true;   
+  User.findById('5bab316ce0a7c75f783cb8a8')
+    .then(user => {
+      req.session.isLoggedIn = true;
+      req.session.user = user;
+      req.session.save(err => {
+        console.log(err);
+        res.redirect('/');
+      });
+    })
+    .catch(err => console.log(err));
+};
+
+exports.postLogout = (req, res, next) => {
+  req.session.destroy(err => {
+    console.log(err);
     res.redirect('/');
-  };
+  });
+};
