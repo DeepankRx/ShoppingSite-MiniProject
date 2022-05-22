c15dfdb40780ce9d0bfc8acb646c1502a5a6f2be
DeepankRx <deepankpushpad10@gmail.com>
2022-05-22 23:03:03 +0530

    mvc pattern

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
-
+const bodyParser = require("body-parser");
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.post('/product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/');
+})  
 const port = 5000;
 app.listen(port,()=>{
     console.log(`Server is running on port ${port}`);
---
+++ frontend/src/Components/NavigationBar.js
@@ -0,0 +1,20 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import {Nav, Navbar,Container} from 'react-bootstrap';
+function NavigationBar() {
+  return (
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">My Shopping Site</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
+  );
+}
+
+export default NavigationBar;
